727a0ed3251658dc75cea857396dd52c81ab8262
Dominik Tomalczyk <dominik.smalczyk@gmail.com>
2021-11-13 14:01:51 +0100

    adding a setup file methods

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from datetime import datetime, timedelta, date
 from typing import List, Dict
 import requests
 import json
+import os.path
 
 from pycoingecko import CoinGeckoAPI
+import openpyxl
 
 from setUp import miner_address
 
@@ -40,7 +42,7 @@ class Miner:
 
         res = requests.get(self.base_link + self.miner_address + "/currentStats")
         file = json.loads(res.text)
-        return round(float(file['data']['coinsPerMin']) * 24 * 60 , 5)
+        return round(float(file['data']['coinsPerMin']) * 24 * 60, 5)
 
     def get_min_payment_threshold(self) -> float:
         """ Returns minimum payment threshold [ETH] """
@@ -87,18 +89,56 @@ class Miner:
         """
         res = requests.get(self.base_link + self.miner_address + "/dashboard")
         file = json.loads(res.text)
-        return round(float(file['data']['statistics'][0]['reportedHashrate'])/1000000, 1)
+        return round(float(file['data']['statistics'][0]['reportedHashrate']) / 1000000, 1)
+
 
 class Data_Excel:
-    pass
+
+    def __init__(self):
+        self.setup_data_excel_file()
+
+    def setup_data_excel_file(self) -> None:
+        """
+        Sets a file to write a data if not exist
+        :return:
+        """
+        if os.path.isfile('mining_data.xlsx'):  # check if file exist
+            return
+        wb = openpyxl.Workbook()  # creating new file
+        sheet = wb.active
+        sheet['A1'] = "Date:"
+        sheet['A2'] = "Current Hashrate:"
+        sheet['A3'] = "Unpaid ETH:"
+        sheet['A4'] = "Daily ETH:"
+        sheet["A5"] = "Sum of payouts:"
+        sheet["A6"] = "Days to next payout:"
+        self.stretch_cells(sheet)  # stretch cells
+        wb.save(filename="mining_data.xlsx")  # save file
+
+    def stretch_cells(self, sheet: openpyxl.worksheet.worksheet.Worksheet) -> None:
+        """
+        Stretchs cells in first column
+        :param sheet: openpyxl.worksheet.worksheet.Worksheet
+        :return: None
+        """
+        dims = {}
+        for row in sheet.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), len(str(cell.value))))
+        for col, value in dims.items():
+            sheet.column_dimensions[col].width = value
+
 
 if __name__ == "__main__":
-    m = Miner(miner_address)
-    print(m.get_unpaid_eth())
-    print(m.get_sum_payouts())
-    print(m.get_daily_eth())
-    print(m.get_min_payment_threshold())
-    print(m.days_to_next_payout())
-    print(m.date_of_next_payout())
-    print(m.get_current_hashrate())
-    print(m.get_list_of_payouts())
+    # m = Miner(miner_address)
+    # print(m.get_unpaid_eth())
+    # print(m.get_sum_payouts())
+    # print(m.get_daily_eth())
+    # print(m.get_min_payment_threshold())
+    # print(m.days_to_next_payout())
+    # print(m.date_of_next_payout())
+    # print(m.get_current_hashrate())
+    # print(m.get_list_of_payouts())
+    de = Data_Excel()
+    de.setup_data_excel_file()
